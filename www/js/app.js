9dd72fbdb23448ec4bf73971bb988a9825fd2851
shankari <shankari@eecs.berkeley.edu>
2020-10-28 09:06:41 -0700

    Upgrade to the latest versions of everything (#19)
    * Upgrade to the latest versions of everything
    
    This is essentially a port of all the native changes included in v2.9.0 of the
    main e-mission-phone repo.
    https://github.com/e-mission/e-mission-phone/releases/tag/v2.9.0
    
    Notably:
    - Upgrade to API 29 on android and iOS 13 on iOS
    - Upgrade to cordova-android@9.0.0 and cordova-ios@6.1.0
    - Upgrade to the most recent version of all plugins
    - Improved robustness on both android and iOS (https://github.com/e-mission/e-mission-data-collection/releases/tag/v1.5.0)
    - Setup scripts to make it easy to build
    - Continuous integration using the setup scripts to test that everything continues to work
    
    However, it does *not* include all the javascript fixes.
    The javascript fixes were merged over and the conflicts resolved as follows:
    
    ```
    www/css/intro.css: e-mission-base
    www/css/style.css: upstream
    www/index.html: upstream for the CSP, retain for everything else
    www/js/app.js: e-mission-base
    www/js/controllers.js: e-mission-base
    www/js/plugin/logger.js: upstream
    www/js/splash/updatecheck.js: upstream (with minor changes to remove the translation)
    ```
    
    This does not include the i18n fixes. I will merge those in a separate PR.
    
    * Remove all references to the "cordovabuild" and "serve" options
    
    Since we don't really have separate serve and build options in this fork
    
    * Remove the other icon references
    
    Since they are only created by `ionic cordova resources`
    
    * Restore teardown_shared because it is called from teardown_native
    
    + fix the package name in google services

--- www/js/app.js
+++ www/js/app.js
@@ -3,6 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'emission' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
+// 'emission.services' is found in services.js
 // 'emission.controllers' is found in controllers.js
 'use strict';
 
@@ -32,11 +33,6 @@ angular.module('emission', ['ionic',
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     Logger.log("ionicPlatform is ready");
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
@@ -47,8 +43,9 @@ angular.module('emission', ['ionic',
         Logger.log("defaultConfig = "+JSON.stringify(defaultConfig));
         window.cordova.plugins.BEMConnectionSettings.setSettings(defaultConfig);
     }).catch(function(err) {
-        Logger.log("error "+JSON.stringify(err)+" reading or setting defaults, giving up");
+        Logger.displayError("Error reading or setting connection defaults", err);
     });
+    cordova.plugin.http.setDataSerializer('json');
   });
   console.log("Ending run");
 })
